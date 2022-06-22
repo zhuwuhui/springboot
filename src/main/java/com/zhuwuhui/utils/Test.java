723077337f170d8c31a0c155247c85324e077e20
zhuwuhui <1565019417@qq.com>
2022-06-22 09:38:05 +0800

    Update Test.java
    测试

--- src/main/java/com/zhuwuhui/utils/Test.java
+++ src/main/java/com/zhuwuhui/utils/Test.java
@@ -8,11 +8,5 @@ public class Test {
         System.out.println("1");
         System.out.println("3");
         System.out.println("2");
-        System.out.println("4");
-        System.out.println("4");
-        System.out.println("local");
-        System.out.println("local");
-        System.out.println("local");
-        System.out.println("我");
     }
 }
