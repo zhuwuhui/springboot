6720d34e5fdd165db5d642603c65a318df500ccc
zhuwuhui <1565019417@qq.com>
2022-06-22 09:24:44 +0800

    q

--- src/main/java/com/zhuwuhui/utils/Test.java
+++ src/main/java/com/zhuwuhui/utils/Test.java
@@ -6,5 +6,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("local");
+        System.out.println("local");
+        System.out.println("local");
     }
 }
