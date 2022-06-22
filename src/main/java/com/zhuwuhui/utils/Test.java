9c9f8de45e1d22048568c32d70377d560c157dfc
zhuwuhui <1565019417@qq.com>
2022-06-22 09:37:35 +0800

    本地测试

--- src/main/java/com/zhuwuhui/utils/Test.java
+++ src/main/java/com/zhuwuhui/utils/Test.java
@@ -5,6 +5,11 @@ import com.zhuwuhui.entity.User;
 public class Test {
 
     public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("4");
+        System.out.println("4");
         System.out.println("local");
         System.out.println("local");
         System.out.println("local");
