eb12edebd1eddfc118e825464b7484d5f6c1c01b
zhuwuhui <1565019417@qq.com>
2022-06-22 09:42:16 +0800

    Update Test.java
    a

--- src/main/java/com/zhuwuhui/utils/Test.java
+++ src/main/java/com/zhuwuhui/utils/Test.java
@@ -6,8 +6,5 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("我");
-        System.out.println("1");
-        System.out.println("3");
-        System.out.println("2");
     }
 }
