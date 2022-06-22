1a5fad827a853c41ece34e1d3df15d705067e013
zhuwuhui <1565019417@qq.com>
2022-06-22 09:59:25 +0800

    Update Test.java

--- src/main/java/com/zhuwuhui/utils/Test.java
+++ src/main/java/com/zhuwuhui/utils/Test.java
@@ -5,9 +5,6 @@ import com.zhuwuhui.entity.User;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("远程");
-        System.out.println("远程");
-        System.out.println("远程");
         System.out.println("本地");
         System.out.println("本地");
         System.out.println("本地");
